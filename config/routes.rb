35cce3781b814e4b061fac4a48d6831457bbd859
Jason <uniqueacheng@gmail.com>
2021-01-18 16:53:06 +0800

    修改默认路由

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'controller_name#action_name'
   Rails.application.routes.draw do
-    # root 'application#hello'
-    root 'application#goodBye'
+    root 'application#hello'
+    #root 'application#goodBye'
   end
 end
